074d723037bd26b319114365e35adf8ab8b055d4
Ross Buchan <rossbuchan1@googlemail.com>
2022-08-22 16:41:20 +0100

    Create html file and js code for computer game

---
+++ script.js
@@ -0,0 +1,48 @@
+function getComputerChoice(){
+    const valComputerChoice = Math.floor(Math.random()*100);
+    console.log(valComputerChoice);
+    if (valComputerChoice <= 33) {
+        computerChoice = "rock";
+    }
+    else if (valComputerChoice > 33 && valComputerChoice <= 66) {
+        computerChoice = "paper";
+    }
+    else {
+        computerChoice = "scissors";
+    }
+    return computerChoice; 
+}
+
+
+function playRound(computerChoice, playerChoice) {
+    if (computerChoice == playerChoice.toLowerCase()) {
+        result = "You Draw! Play again";
+    }
+    if (computerChoice == "rock") {
+        if (playerChoice.toLowerCase() == "scissors") {
+            result = "You lose! Rock beats Scissors";
+        }
+        if (playerChoice.toLowerCase() == "paper") {
+            result = "You win! Paper beats Rock";
+        }
+    }
+    if (computerChoice == "paper") {
+        if (playerChoice.toLowerCase() == "rock") {
+            result = "You lose! Paper beats Rock";
+        }
+        if (playerChoice.toLowerCase() == "scissors") {
+            result = "You win! Scissors beats Paper";
+        }
+    }
+    if (computerChoice == "scissors") {
+        if (playerChoice.toLowerCase() == "rock") {
+            result = "You win! Rock beats Scissors";
+        }
+        if (playerChoice.toLowerCase() == "paper") {
+            result = "You lose! Scissors beats Paper"
+        }
+    }
+    console.log(result);
+}
+
+playRound(getComputerChoice(), "rock")
